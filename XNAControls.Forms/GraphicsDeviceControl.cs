f3b6f1504e5a479609ab75bebd787be10599907e
deaddog <mikkelslsl@gmail.com>
2013-08-16 14:32:24 +0200

    Added XNAControls.Forms project and library in order to support WinForms based applications.

---
+++ XNAControls.Forms/GraphicsDeviceControl.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Color = System.Drawing.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace XNAControls
+{
+    /// <summary>
+    /// Allows the usage of a control as an XNA canvas.
+    /// Derived classes can override the Update and Draw methods to add content.
+    /// </summary>
+    public abstract class GraphicsDeviceControl : System.Windows.Forms.Control
+    {
+        private ContentManager content;
+        private SpriteBatch spritebatch;
+
+        private GraphicsDeviceService graphicsDeviceService;
+        private ServiceContainer services = new ServiceContainer();
+        
+        private DateTime start = DateTime.Now;
+        private DateTime last = DateTime.Now;
+
+        /// <summary>
+        /// Gets a <see cref="GraphicsDevice"/> that can be used to draw onto this <see cref="GraphicsDeviceControl"/>.
+        /// </summary>
+        public GraphicsDevice GraphicsDevice
+        {
+            get { return graphicsDeviceService.GraphicsDevice; }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> associated with this <see cref="GraphicsDeviceControl"/>.
+        /// </summary>
+        public IServiceProvider Services
+        {
+            get { return services; }
+        }
+
+        public GraphicsDeviceControl(string contentRoot, GraphicsProfile graphicsProfile = GraphicsProfile.HiDef)
+            : base()
+        {
+            if (!DesignMode)
+            {
+                graphicsDeviceService = GraphicsDeviceService.AddRef(Handle, ClientSize.Width, ClientSize.Height, graphicsProfile);
+                services.AddService<IGraphicsDeviceService>(graphicsDeviceService);
+
+                content = new ContentManager(Services, contentRoot);
+                spritebatch = new SpriteBatch(GraphicsDevice);
+
+                Application.Idle += delegate { this.Invalidate(); };
+
+                Initialize();
+            }
+        }
+
+        /// <summary>
+        /// Disposes the control.
+        /// </summary>
+        protected sealed override void Dispose(bool disposing)
+        {
+            if (disposing)
+                content.Unload();
+
+            if (graphicsDeviceService != null)
+            {
+                graphicsDeviceService.Release(disposing);
+                graphicsDeviceService = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Redraws the control in response to a WinForms paint message.
+        /// </summary>
+        protected sealed override void OnPaint(PaintEventArgs e)
+        {
+            DateTime current = DateTime.Now;
+            Update(new GameTime(current - start, current - last));
+
+            string beginDrawError = BeginDraw();
+
+            if (string.IsNullOrEmpty(beginDrawError))
+            {
+                current = DateTime.Now;
+                Draw(spritebatch, new GameTime(current - start, current - last));
+                EndDraw();
+                last = current;
+            }
+            else
+            {
+                // If BeginDraw failed, show an error message using System.Drawing.
+                PaintUsingSystemDrawing(e.Graphics, beginDrawError);
+            }
+        }
+
+        private string BeginDraw()
+        {
+            // If we have no graphics device, we must be running in the designer.
+            if (graphicsDeviceService == null)
+            {
+                return Text + "\n\n" + GetType();
+            }
+
+            // Make sure the graphics device is big enough, and is not lost.
+            string deviceResetError = HandleDeviceReset();
+
+            if (!string.IsNullOrEmpty(deviceResetError))
+            {
+                return deviceResetError;
+            }
+
+            // Many GraphicsDeviceControl instances can be sharing the same
+            // GraphicsDevice. The device backbuffer will be resized to fit the
+            // largest of these controls. But what if we are currently drawing
+            // a smaller control? To avoid unwanted stretching, we set the
+            // viewport to only use the top left portion of the full backbuffer.
+            Viewport viewport = new Viewport();
+
+            viewport.X = 0;
+            viewport.Y = 0;
+
+            viewport.Width = ClientSize.Width;
+            viewport.Height = ClientSize.Height;
+
+            viewport.MinDepth = 0;
+            viewport.MaxDepth = 1;
+
+            GraphicsDevice.Viewport = viewport;
+
+            return null;
+        }
+        private void EndDraw()
+        {
+            try
+            {
+                Rectangle sourceRectangle = new Rectangle(0, 0, ClientSize.Width,
+                                                                ClientSize.Height);
+
+                GraphicsDevice.Present(sourceRectangle, null, this.Handle);
+            }
+            catch
+            {
+                // Present might throw if the device became lost while we were
+                // drawing. The lost device will be handled by the next BeginDraw,
+                // so we just swallow the exception.
+            }
+        }
+
+        private string HandleDeviceReset()
+        {
+            bool deviceNeedsReset = false;
+
+            switch (GraphicsDevice.GraphicsDeviceStatus)
+            {
+                case GraphicsDeviceStatus.Lost:
+                    // If the graphics device is lost, we cannot use it at all.
+                    return "Graphics device lost";
+
+                case GraphicsDeviceStatus.NotReset:
+                    // If device is in the not-reset state, we should try to reset it.
+                    deviceNeedsReset = true;
+                    break;
+
+                default:
+                    // If the device state is ok, check whether it is big enough.
+                    PresentationParameters pp = GraphicsDevice.PresentationParameters;
+
+                    deviceNeedsReset = (ClientSize.Width > pp.BackBufferWidth) ||
+                                       (ClientSize.Height > pp.BackBufferHeight);
+                    break;
+            }
+
+            // Do we need to reset the device?
+            if (deviceNeedsReset)
+            {
+                try
+                {
+                    graphicsDeviceService.ResetDevice(ClientSize.Width,
+                                                      ClientSize.Height);
+                }
+                catch (Exception e)
+                {
+                    return "Graphics device reset failed\n\n" + e;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Draws a message on the face of the control.
+        /// Only called if drawing is not performed directly by XNA (on error or in the Visual Studio Designer).
+        /// </summary>
+        /// <param name="graphics">A <see cref="Graphics"/> item associated with the triggering Paint event.</param>
+        /// <param name="text">The (error)message to display on the control.</param>
+        protected virtual void PaintUsingSystemDrawing(Graphics graphics, string text)
+        {
+            graphics.Clear(Color.CornflowerBlue);
+
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                using (StringFormat format = new StringFormat())
+                {
+                    format.Alignment = StringAlignment.Center;
+                    format.LineAlignment = StringAlignment.Center;
+
+                    graphics.DrawString(text, Font, brush, ClientRectangle, format);
+                }
+            }
+        }
+
+        /// <summary>
+        /// All paint-background messages are ignored.
+        /// </summary>
+        /// <param name="pevent">The <see cref="PaintEventArgs"/> associated with the event.</param>
+        protected sealed override void OnPaintBackground(PaintEventArgs pevent)
+        {
+        }
+        
+
+        /// <summary>
+        /// When overridden in a derived class, initializes the <see cref="GraphicsDeviceControl"/>.
+        /// </summary>
+        protected virtual void Initialize() { }
+        /// <summary>
+        /// Called when the <see cref="GraphicsDeviceControl"/> needs to be drawn. Override this method with component-specific drawing code.
+        /// </summary>
+        /// <param name="spritebatch">A <see cref="SpriteBatch"/> enabled simple drawing of groups of sprites.</param>
+        /// <param name="gameTime">Time elapsed since the last call to Draw.</param>
+        protected virtual void Draw(SpriteBatch spritebatch, GameTime gameTime) { }
+        /// <summary>
+        /// Called when the <see cref="GraphicsDeviceControl"/> needs to be updated. Override this method with component-specific update code.
+        /// </summary>
+        /// <param name="gameTime">Time elapsed since the last call to Update.</param>
+        protected virtual void Update(GameTime gameTime) { }
+    }
+}
---
+++ XNAControls.Forms/GraphicsDeviceService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAControls
+{
+    internal class GraphicsDeviceService : IGraphicsDeviceService
+    {
+        private static GraphicsProfile profile = GraphicsProfile.HiDef;
+
+        private static GraphicsDeviceService singletonInstance;
+        private static int referenceCount;
+
+        private GraphicsDevice graphicsDevice;
+        private PresentationParameters parameters;
+
+        private GraphicsDeviceService(IntPtr windowHandle, int width, int height, GraphicsProfile graphicsProfile)
+        {
+            parameters = new PresentationParameters();
+
+            parameters.BackBufferWidth = Math.Max(width, 1);
+            parameters.BackBufferHeight = Math.Max(height, 1);
+            parameters.BackBufferFormat = SurfaceFormat.Color;
+            parameters.DepthStencilFormat = DepthFormat.Depth24;
+            parameters.DeviceWindowHandle = windowHandle;
+            parameters.PresentationInterval = PresentInterval.Immediate;
+            parameters.IsFullScreen = false;
+
+            graphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter, profile, parameters);
+        }
+
+        /// <summary>
+        /// Gets a reference to the singleton instance.
+        /// </summary>
+        public static GraphicsDeviceService AddRef(IntPtr windowHandle, int width, int height, GraphicsProfile graphicsProfile)
+        {
+            if (profile != graphicsProfile && referenceCount > 0)
+                throw new InvalidOperationException("All controls must use similar graphics profile. The current is " + profile.ToString());
+
+            // Increment the "how many controls sharing the device" reference count.
+            if (Interlocked.Increment(ref referenceCount) == 1)
+            {
+                profile = graphicsProfile;
+                singletonInstance = new GraphicsDeviceService(windowHandle, width, height, graphicsProfile);
+            }
+
+            return singletonInstance;
+        }
+
+        /// <summary>
+        /// Releases a reference to the singleton instance.
+        /// </summary>
+        public void Release(bool disposing)
+        {
+            // Decrement the "how many controls sharing the device" reference count.
+            if (Interlocked.Decrement(ref referenceCount) == 0)
+            {
+                if (disposing)
+                {
+                    if (DeviceDisposing != null)
+                        DeviceDisposing(this, EventArgs.Empty);
+
+                    graphicsDevice.Dispose();
+                }
+
+                graphicsDevice = null;
+            }
+        }
+
+        /// <summary>
+        /// Resets the graphics device to whichever is bigger out of the specified
+        /// resolution or its current size. This behavior means the device will
+        /// demand-grow to the largest of all its GraphicsDeviceControl clients.
+        /// </summary>
+        public void ResetDevice(int width, int height)
+        {
+            if (DeviceResetting != null)
+                DeviceResetting(this, EventArgs.Empty);
+
+            parameters.BackBufferWidth = Math.Max(parameters.BackBufferWidth, width);
+            parameters.BackBufferHeight = Math.Max(parameters.BackBufferHeight, height);
+
+            graphicsDevice.Reset(parameters);
+
+            if (DeviceReset != null)
+                DeviceReset(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Gets the current graphics device.
+        /// </summary>
+        public GraphicsDevice GraphicsDevice
+        {
+            get { return graphicsDevice; }
+        }
+
+#pragma warning disable 67
+
+        public event EventHandler<EventArgs> DeviceCreated;
+        public event EventHandler<EventArgs> DeviceDisposing;
+        public event EventHandler<EventArgs> DeviceReset;
+        public event EventHandler<EventArgs> DeviceResetting;
+    }
+}
