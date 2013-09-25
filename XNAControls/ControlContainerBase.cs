323a1bfa4c52dcf78bf58b4b988b88fc79d86950
deaddog <mikkelslsl@gmail.com>
2013-09-25 15:00:51 +0200

    Copied ControlCollection to container.

--- XNAControls/ControlContainerBase.cs
+++ XNAControls/ControlContainerBase.cs
@@ -8,9 +8,85 @@ namespace XNAControls
 {
     public class ControlContainerBase : Control
     {
+        private bool contentLoaded;
+        private ControlCollection controls;
+
         public ControlContainerBase(float initialwidth, float initialheight)
             : base(initialwidth, initialheight)
         {
+            this.contentLoaded = false;
+            this.controls = new ControlCollection(this);
+        }
+
+        public ControlCollection Controls
+        {
+            get { return controls; }
+        }
+
+        public class ControlCollection : IEnumerable<Control>
+        {
+            private ControlContainerBase container;
+            private List<Control> list;
+
+            internal ControlCollection(ControlContainerBase container)
+            {
+                this.container = container;
+                this.list = new List<Control>();
+            }
+
+            public Control this[int index]
+            {
+                get { return list[index]; }
+            }
+
+            public int Count
+            {
+                get { return list.Count; }
+            }
+
+            public void Add(Control control)
+            {
+                list.Add(control);
+                if (container.contentLoaded)
+                    //Load control resources
+                throw new NotImplementedException();
+            }
+            public bool Remove(Control control)
+            {
+                if (list.Contains(control))
+                {
+                    if (container.contentLoaded)
+                        //Unload control resources
+                    throw new NotImplementedException();
+                    list.Remove(control);
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public bool Contains(Control control)
+            {
+                return list.Contains(control);
+            }
+
+            #region IEnumerable<Control> Members
+
+            public IEnumerator<Control> GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+
+            #endregion
+
+            #region IEnumerable Members
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+
+            #endregion
         }
     }
 }
