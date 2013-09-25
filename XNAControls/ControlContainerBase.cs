edc1e0b70dcdb54e9d7249066230d4c7aff9972c
deaddog <mikkelslsl@gmail.com>
2013-09-25 14:35:50 +0200

    Added container control file.

---
+++ XNAControls/ControlContainerBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAControls
+{
+    public class ControlContainerBase : Control
+    {
+        public ControlContainerBase(float initialwidth, float initialheight)
+            : base(initialwidth, initialheight)
+        {
+        }
+    }
+}
